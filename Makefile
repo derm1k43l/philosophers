19e394e69b54c65b392c15a48f66c0dc96cb6caf
Mihai Rusu <mrusu@3-E-8.42heilbronn.de>
2024-06-13 10:10:55 +0200

    upload project files

---
+++ Makefile
@@ -0,0 +1,64 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/13 09:21:56 by mrusu             #+#    #+#              #
+#    Updated: 2024/06/13 09:22:12 by mrusu            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+# COMPILER
+CC = cc
+
+# FLAGS FOR COMPILATION
+FLAGS = -Wall -Wextra -Werror -g -pthread #-Ofast -Wunreachable-code -fsanitize=address
+
+# philo DIRECTORIES
+SRC_DIR = src
+INC_DIR = inc
+OBJ_DIR = obj
+
+# FdF EXECUTABLE NAME
+NAME = philo
+
+# HEADER FILE
+HEADER = $(INC_DIR)/philo.h
+
+# LIST OF SOURCE FILES
+SRC = $(SRC_DIR)/main.c $(SRC_DIR)/utils.c $(SRC_DIR)/error.c $(SRC_DIR)/data.c $(SRC_DIR)/input.c \
+		$(SRC_DIR)/simulation0.c $(SRC_DIR)/simulation1.c
+OBJS = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC))
+
+# COLORS
+GREEN = \033[0;32m
+NC = \033[0m
+
+# ALL RULE
+all: $(NAME)
+	@echo "$(GREEN)Philosophers build successful$(NC)"
+
+$(NAME): $(OBJS)
+	@$(CC) $(FLAGS) -I$(INC_DIR) $(OBJS) -o $(NAME)
+
+# RULES FOR CREATING OBJECT FILES
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(FLAGS) -I$(INC_DIR) -c $< -o $@
+
+# RULES TO CLEAN EVERYTHING
+clean:
+	@rm -rf $(OBJ_DIR)
+	@echo "$(GREEN)Philosophers cleaned$(NC)"
+
+fclean: clean
+	@rm -f $(NAME)
+	@echo "$(GREEN)Philosophers fcleaned$(NC)"
+
+# RULES FOR RECOMPILATION
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ inc/philo.h
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 09:47:35 by mrusu             #+#    #+#             */
+/*   Updated: 2024/06/13 09:47:59 by mrusu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// Standard
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <errno.h>
+
+// Colors
+# define R "\033[1;31m"
+# define G "\033[0;32m"
+# define Y "\033[0;33m"
+# define B "\033[0;34m"
+# define W "\033[0;37m"
+# define DEF "\033[m"
+
+// Typdef
+typedef pthread_mutex_t	t_mtx;
+
+// Forward declaration of structs
+typedef struct s_philo	t_philo;
+
+// **********************====ENUM====*********************
+// code for philo status
+typedef enum e_status
+{
+	EATING,
+	SLEEPING,
+	THINKING,
+	TAKE_RIGHT_FORK,
+	TAKE_LEFT_FORK,
+	DIED,
+}	t_status;
+
+// code for time
+typedef enum e_time
+{
+	SECOND,
+	MILLISECOND,
+	MICROSECOND,
+}	t_time;
+
+// **********************====STRUCT====*********************
+// Strucutre for simulation enviroment
+typedef struct s_simulation
+{
+	long		philo_nbr;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		time_to_die;
+	long		limit_meals;
+	long		start_time;
+	long		threads_running_nbr;
+	bool		simulation_running;
+	bool		sync_ready;
+	t_mtx		sim_mutex;
+	t_mtx		print_mtx;
+	t_mtx		*forks;
+	t_philo		*philosophers;
+	pthread_t	monitor;
+}	t_simulation;
+
+// Strucutre for Philosophers atributes
+typedef struct s_philo
+{
+	long			id;
+	long			last_meal_time;
+	long			meals_index;
+	bool			full;
+	t_mtx			*left_fork;
+	t_mtx			*right_fork;
+	t_mtx			philo_mutex;
+	pthread_t		id_thread;
+	t_simulation	*simulation;
+}	t_philo;
+
+// **********************====FUNCTION DECLARATION====*********************
+// main.c
+void	*monitoring_dinner(void *data);
+
+// utils.c
+void	*ft_malloc(size_t bytes);
+void	clean(t_simulation *simulation);
+long	ft_gettime(t_time code);
+void	ft_usleep(t_simulation *simulation, long u);
+void	print_status(t_philo *philo, t_status status);
+
+// input.c
+void	input_check(t_simulation *simulation, char **av);
+long	ft_atol(char *str);
+char	*parse_input(char *str);
+
+// data.c
+void	data_init(t_simulation *simulation);
+void	philo_init(t_simulation *simulation);
+void	assign_forks(t_philo *philo, t_mtx *forks);
+
+// error.c
+void	error_exit(const char *error);
+bool	philo_died(t_philo *philo);
+
+// set_get.c
+bool	simulation_status(t_simulation *simulation);
+void	ft_usleep(t_simulation *simulation, long usecond);
+bool	simulation_status(t_simulation *simulation);
+
+// simulation0.c
+void	start_simulation(t_simulation *simulation);
+void	*dinner_routin(void *data);
+void	eat(t_philo *philo);
+bool	check_bool(t_mtx *mutex, bool *value);
+
+// simulation1.c
+void	wait_sync(t_simulation *simulation);
+void	desync(t_philo *philo);
+void	*unus_philosophus(void *arg);
+
+#endif
