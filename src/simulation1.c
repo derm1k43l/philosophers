643f8f983dbb34c724094e7e3463f62f5adec998
Mihai Rusu <mrusu@3-E-8.42heilbronn.de>
2024-06-13 10:36:32 +0200

    upload current stateeee

--- src/simulation1.c
+++ src/simulation1.c
@@ -6,7 +6,7 @@
 /*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 10:24:35 by mrusu             #+#    #+#             */
-/*   Updated: 2024/06/13 10:34:13 by mrusu            ###   ########.fr       */
+/*   Updated: 2024/06/13 10:36:20 by mrusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ void	wait_sync(t_simulation *simulation)
 	pthread_mutex_unlock(&simulation->sim_mutex);
 }
 
-// staging for avoiding deadlocks
-// If the number of philosophers is even, desynchronize the even ID philosophers by 2e4/20ms
+// Staging for avoiding deadlocks
+// If the number of philosophers is even, 
+// desynchronize the even ID philosophers by 2e4/20ms
 // If the number of philosophers is odd, calculate the thinking time
 // Desynchronize by sleeping for half of the calculated thinking time
 void	desync(t_philo *philo)
@@ -50,4 +51,34 @@ void	desync(t_philo *philo)
 			ft_usleep(philo->simulation, t_think * 0.5);
 		}
 	}
+}
+
+bool	simulation_status(t_simulation *simulation)
+{
+	bool	status;
+
+	pthread_mutex_lock(&simulation->sim_mutex);
+	status = simulation->simulation_running;
+	pthread_mutex_unlock(&simulation->sim_mutex);
+	return (status);
+}
+
+void	*unus_philosophus(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	wait_sync(philo->simulation);
+	pthread_mutex_lock(&philo->simulation->sim_mutex);
+	philo->last_meal_time = ft_gettime(MILLISECOND);
+	philo->simulation->threads_running_nbr++;
+	pthread_mutex_unlock(&philo->simulation->sim_mutex);
+	print_status(philo, TAKE_RIGHT_FORK);
+	while (simulation_status(philo->simulation))
+	{
+		ft_usleep(philo->simulation, philo->simulation->time_to_die);
+		if (!simulation_status(philo->simulation))
+			break ;
+	}
+	return (NULL);
 }
